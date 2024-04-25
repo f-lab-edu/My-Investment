c7e7ed42c1c80ad73f16f953de7f36dffa868f37
Pdongjae <kyp4810@gmail.com>
2024-04-25 21:13:48 +0900

    [#1] ⭐Feat : WebSecurityConfig @Bean 해당 기능 설명

--- src/main/java/com/myinvestment/config/WebSecurityConfig.java
+++ src/main/java/com/myinvestment/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,30 +23,34 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    /*
+     web.ignoring().requestMatchers
+     : () 안에 해당하는 요청들을 Spring Security의 필터 체인에서 제외시켜줍니다.
+     /configuration/security 는 보안 구성과 관련된 경로입니다.
+     /webjars/** 는 웹 애플리케이션에서 사용되는 JS, CSS 등의 라이브러리를 포함합니다.
+     */
     @Bean
     public WebSecurityCustomizer ignoringCustomizer() {
         return (web) -> web.ignoring()
-                .requestMatchers("/h2-console/**",
-                        "/configuration/ui",
+                .requestMatchers(
                         "/configuration/security",
                         "/webjars/**");
     }
 
+
+    /*
+    * http.authorizeHttpRequests(request -> request.requestMatchers("/").permitAll())
+    * 모든 요청에 대해 접근을 허용합니다.
+    * */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf((csrf) -> csrf.disable());
-
         http
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/")
                         .permitAll()
                 )
                 .authorizeHttpRequests(request -> request.anyRequest().permitAll());
-//                .addFilterBefore();
 
         return http.build();
     }
